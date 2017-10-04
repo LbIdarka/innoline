0537ee672facbd6eab48726c869d4bdb40e11c2b
Alena Nikolaenko <lbidarka@gmail.com>
2017-10-04 22:52:41 +0300

    мелкие правки

--- js/script.js
+++ js/script.js
@@ -2,9 +2,7 @@
 
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
-var bgPosition = document.querySelector('.page-current');
 
-// bgPosition.classList.remove('page-current--nojs');
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function() {
@@ -20,10 +18,10 @@ navToggle.addEventListener('click', function() {
 //======= функция для фиксированной навигации =======//
 
 window.onscroll = function() {//событие скрола
-  var stickyMenu = document.querySelector(".menu"); //задаем переменную для элемента, который будет "липнуть"
-  var scrolled = window.pageYOffset; //проверяем состояние скрола
-  var logoRed = document.querySelector('.logo__img-color');
-  var logo = document.querySelector('.logo__img');
+var stickyMenu = document.querySelector(".menu"); //задаем переменную для элемента, который будет "липнуть"
+var scrolled = window.pageYOffset; //проверяем состояние скрола
+var logoRed = document.querySelector('.logo__img-color');
+var logo = document.querySelector('.logo__img');
 
   if(scrolled) { //если скрол больше или равен отступу меню + слоган, то добавляем класс с position: fixed;
     stickyMenu.classList.add("menu--fixed");
