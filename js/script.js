07f1a364cd30472172ee8179fd3dbc284fbd9a71
Alena Nikolaenko <lbidarka@gmail.com>
2017-11-18 15:21:40 +0300

    выпадающее меню

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 //======= гамбургерное меню =======//
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var submenuParent = navMain.querySelector('.main-nav__item');
+var submenuToggle = navMain.querySelector('.main-nav-item__btn');
+var submenu = document.querySelector('.submenu');
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -13,6 +16,44 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+
+// submenuToggle.addEventListener('click', function() {
+//   if (submenu.classList.contains('submenu--closed')) {
+//     submenu.classList.remove('submenu--closed');
+//     submenu.classList.add('submenu--opened');
+//   } else {
+//     submenu.classList.remove('submenu--opened');
+//     submenu.classList.add('submenu--closed');
+//   }
+// });
+
+
+// var dropdown = document.querySelectorAll('.main-nav__item');
+// var dropdownArray = Array.prototype.slice.call(dropdown,0);
+// dropdownArray.forEach(function(el){
+// 	var button = el.querySelector('.dropdown__link'),
+// 			menu = el.querySelector('.submenu'),
+// 			arrow = button.querySelector('.main-nav-item__btn');
+
+// 	button.onclick = function(event) {
+// 		if(!menu.hasClass('submenu--opened')) {
+// 			menu.classList.add('submenu--opened');
+// 			menu.classList.remove('submenu--closed');
+// 			arrow.classList.add('main-nav-item__btn--opened');
+// 			arrow.classList.remove('main-nav-item__btn--down');
+// 			event.preventDefault();
+// 		}
+// 		else {
+// 			menu.classList.remove('submenu--opened');
+// 			menu.classList.add('submenu--closed');
+// 			arrow.classList.remove('main-nav-item__btn--opened');
+// 			arrow.classList.add('main-nav-item__btn--down');
+// 			event.preventDefault();
+// 		}
+// 	};
+// })
+
 
 //======= функция для фиксированной навигации =======//
 
